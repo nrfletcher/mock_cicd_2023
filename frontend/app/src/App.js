ba18c4e2f535664a41ab12718fafbf00055273eb
riley <nrileyfletcher@gmail.com>
2023-06-05 23:06:47 -0400

    stuff

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,16 +1,12 @@
-import './resources/App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { CarInfoSection } from './components/CarInfoSection';
+
 import 'jquery/dist/jquery.min.js'
+import Button from '@mui/material/Button'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <CarInfoSection />
-      <CarInfoSection />
-      <Footer />
+      <h1>Test</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
