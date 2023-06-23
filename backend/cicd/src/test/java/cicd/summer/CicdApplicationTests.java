7f45b61b6e4d30cfcfe8c2e995d2cf881817e98c
riley <nrileyfletcher@gmail.com>
2023-06-22 20:16:18 -0400

    Unit testing

--- backend/cicd/src/test/java/cicd/summer/CicdApplicationTests.java
+++ backend/cicd/src/test/java/cicd/summer/CicdApplicationTests.java
@@ -3,6 +3,8 @@ package cicd.summer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CicdApplicationTests {
 
@@ -10,4 +12,12 @@ class CicdApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void thisTest() {
+		assertEquals(1, returnOne());
+	}
+
+	int returnOne() {
+		return 1;
+	}
 }
---
+++ backend/cicd/src/test/java/cicd/summer/SampleTests.java
@@ -0,0 +1,13 @@
+package cicd.summer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTests {
+    
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
